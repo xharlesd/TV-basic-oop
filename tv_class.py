8f57c2b2835fd0b92f2d2603fecb294842312ef0
xharlesd <bernidocharlesdavid@gmail.com>
2023-05-17 11:45:18 +0800

    minor changes

--- tv_class.py
+++ tv_class.py
@@ -1,55 +1,55 @@
 class TV:
 
     # constructor
-    def __init__(self, channel = 1, volume_level = 1, status = False):
+    def __init__(television, channel = 1, volume_level = 1, status = False):
         # instance variables
-        self.channel = channel
-        self.volume_level = volume_level
-        self.status = status
+        television.channel = channel
+        television.volume_level = volume_level
+        television.status = status
 
     # turn on/off TV
-    def turn_on_TV(self):
-        self.status = True
-    def turn_off_TV(self):
-        self.status = False
+    def turn_on_TV(television):
+        television.status = True
+    def turn_off_TV(television):
+        television.status = False
 
     # get TV channel
-    def get_channel(self):
-        return self.channel
+    def get_channel(television):
+        return television.channel
     
     # Set TV channel
-    def set_channel(self, channel):
-        if self.status and 1 <= channel <= 120:
-            self.channel = channel
+    def set_channel(television, channel):
+        if television.status and 1 <= channel <= 120:
+            television.channel = channel
 
     # get TV volume
-    def get_volume_level(self):
-        return self.volume_level
+    def get_volume_level(television):
+        return television.volume_level
 
     # Set TV volume
-    def set_volume_level(self, volume_level):
-        if self.status and 1 <= volume_level <= 7:
-            self.volume_level = volume_level
+    def set_volume_level(television, volume_level):
+        if television.status and 1 <= volume_level <= 7:
+            television.volume_level = volume_level
 
     # switch channel up 
-    def channel_up(self, channel):
-        if self.status and 1 <= channel <= 120:
-            self.channel += 1
+    def channel_up(television, channel):
+        if television.status and 1 <= channel <= 120:
+            television.channel += 1
 
     # switch channel down
-    def channel_down(self, channel):
-        if self.status and 1 <= channel <= 120:
-            self.channel -= 1
+    def channel_down(television, channel):
+        if television.status and 1 <= channel <= 120:
+            television.channel -= 1
 
     # volume up
-    def volume_up(self, volume_level):
-        if self.status and 1 <= volume_level <= 7:
-            self.volume_level += 1
+    def volume_up(television, volume_level):
+        if television.status and 1 <= volume_level <= 7:
+            television.volume_level += 1
 
     # volume down
-    def volume_up(self, volume_level):
-        if self.status and 1 <= volume_level <= 7:
-            self.volume_level -= 1
+    def volume_down(television, volume_level):
+        if television.status and 1 <= volume_level <= 7:
+            television.volume_level -= 1
 
 tv_1 = TV()
 tv_1.turn_on_TV()
@@ -57,3 +57,10 @@ tv_1.set_channel(5)
 tv_1.set_volume_level(6)
 
 print("TV 1's channel is", tv_1.get_channel(), "and volume level is", tv_1.get_volume_level())
+
+tv_2 = TV()
+tv_2.turn_on_TV()
+tv_2.set_channel(7)
+tv_2.set_volume_level(7)
+
+print("TV 2's channel is", tv_2.get_channel(), "and volume level is", tv_2.get_volume_level())
