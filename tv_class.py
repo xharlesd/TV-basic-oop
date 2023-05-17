953e5d0f0c96d1be5e2222f212c8b141953ec6fa
xharlesd <bernidocharlesdavid@gmail.com>
2023-05-17 11:59:47 +0800

    change comments and test run 3 okay

--- tv_class.py
+++ tv_class.py
@@ -7,17 +7,19 @@ class TV:
         television.volume_level = volume_level
         television.status = status
 
-    # turn on/off TV
+    # turn on the TV
     def turn_on_TV(television):
         television.status = True
+
+    # turn off the TV
     def turn_off_TV(television):
         television.status = False
 
     # get TV channel
     def get_channel(television):
-        return television.channel
+        return television.channel  # return channel
     
-    # Set TV channel
+    # set a new TV channel
     def set_channel(television, channel):
         if television.status and 1 <= channel <= 120:
             television.channel = channel
@@ -26,31 +28,32 @@ class TV:
     def get_volume_level(television):
         return television.volume_level
 
-    # Set TV volume
+    # set a new TV volume
     def set_volume_level(television, volume_level):
         if television.status and 1 <= volume_level <= 7:
             television.volume_level = volume_level
 
-    # switch channel up 
+    # increases channel number by 1
     def channel_up(television, channel):
         if television.status and 1 <= channel <= 120:
             television.channel += 1
 
-    # switch channel down
+    # decreases channel number by 1
     def channel_down(television, channel):
         if television.status and 1 <= channel <= 120:
             television.channel -= 1
 
-    # volume up
+    # increases volume level by 1
     def volume_up(television, volume_level):
         if television.status and 1 <= volume_level <= 7:
             television.volume_level += 1
 
-    # volume down
+    # decreases volume level by 1
     def volume_down(television, volume_level):
         if television.status and 1 <= volume_level <= 7:
             television.volume_level -= 1
 
+# test run
 tv_1 = TV()
 tv_1.turn_on_TV()
 tv_1.set_channel(5)
