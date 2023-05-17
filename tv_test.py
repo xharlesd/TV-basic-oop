920695cc0f301f0f554483322829072f16dc79b5
xharlesd <bernidocharlesdavid@gmail.com>
2023-05-17 12:05:01 +0800

    test tv pseudocode

---
+++ tv_test.py
@@ -0,0 +1,14 @@
+# Bernido, Charles David P. | BSCPE 1-5 
+# Assignment 6 in OOP
+
+# import class tv and other modules to be used
+
+# def main
+    # call out TV 1
+    # methods for TV 1
+
+    # call out TV 2
+    # methods for TV 2
+
+#main()
+
